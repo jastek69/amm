6623a35952e7c930ba61aed80fdb633e94e26cff
John <jastek@SEBEK.localdomain>
2023-02-17 03:19:18 -0800

    Update Token.js to SOB token

--- test/Token.js
+++ test/Token.js
@@ -12,7 +12,7 @@ describe('Token', () => {
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
-    token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+    token = await Token.deploy('Sobek', 'SOB', '1000000') // Token is deployed
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
@@ -21,32 +21,32 @@ describe('Token', () => {
   })
 
   describe('Deployment', () => {
-    const name = 'Dapp University'
-    const symbol = 'DAPP'
+    const name = 'Sobek'
+    const symbol = 'SOB'
     const decimals = '18'
     const totalSupply = tokens('1000000')
 
-    it('has correct name', async () => {
-      expect(await token.name()).to.equal(name)
+    it('Has correct name', async () => {    // Read Token name. Check that name is correct ... see Chai matchers: https://ethereum-waffle.readthedocs.io/en/latest/matchers.html 
+        expect(await token.name()).to.equal(name)
     })
 
-    it('has correct symbol', async () => {
-      expect(await token.symbol()).to.equal(symbol)
+    it('Has correct symbol', async () => {  // Read Token symbol. Check that symbol is correct 
+        expect(await token.symbol()).to.equal(symbol)
     })
 
-    it('has correct decimals', async () => {
-      expect(await token.decimals()).to.equal(decimals)
+    it('Has correct decimals', async () => {  // Read Token decimals 
+        expect(await token.decimals()).to.equal(decimals)
     })
 
-    it('has correct total supply', async () => {
-      expect(await token.totalSupply()).to.equal(totalSupply)
+    it('Has correct total supply', async () => {  // Read Token total supply - converts 1000000 to Ether = 1000000000000000000000000
+        expect(await token.totalSupply()).to.equal(totalSupply)
     })
 
-    it('assigns total supply to deployer', async () => {
-      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+    it('Assigns total supply to deployer', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)    
     })
+})    
 
-  })
 
 
   describe('Sending Tokens', () => {
